dca83069658c2724d97b336325dc0a6a33768ba1
mehu-hub <designwithmehedi@gmai.com>
2023-11-24 19:34:25 +0600

    Update Click

--- JS/Script.js
+++ JS/Script.js
@@ -1,7 +1,7 @@
 // write a function for delete or hidden element from UI 
 function deleteCard(){
-    // get click button
-    document.getElementById('btn-cart').addEventListener('click', function(){ 
+    // get click element
+    document.getElementById('card-div').addEventListener('click', function(){ 
         // get parent div
         const getParent = document.getElementById('card-div'); 
         // delete or hidden element
